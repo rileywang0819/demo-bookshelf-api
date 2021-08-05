b86b87da413b5cdbf9ea7236abc8806460f646e5
gogo-wang <rileywang0819@gmail.com>
2021-08-05 22:48:55 +0900

    update form view

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -53,7 +53,8 @@ class FormView extends Component {
   render() {
     return (
       <div id="form-view">
-        <div className="search" style={{'display': 'None'}}>
+        {/* <div className="search" style={{'display': 'None'}}> */}
+        <div className="search">
           <h2>Search</h2>
           <form className="FormView" id="search-form" onSubmit={this.handleSearch}>
             <input type="text" name="search" onChange={this.handleChange}/>
